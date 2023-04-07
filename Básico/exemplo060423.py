ae949053250e02c2a235d7c20d59dcd668273372
Leandro Silva <leandrosdc@outlook.com>
2023-04-06 21:27:42 -0300

    Add files via upload

---
+++ Básico/exemplo060423.py
@@ -0,0 +1,6 @@
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+resultado = num1 + num2
+
+print("A o resultado da soma dos números é: ", resultado)
---
+++ Básico/exemplo2-060423.py
@@ -0,0 +1,5 @@
+nome = str(input("Digite seu nome: "))
+idade = int(input("Digite sua idade: "))
+salario = float(input("Digite seu salário: "))
+
+print("Seu nome é:", nome, "// Sua idade é:", idade, "// Seu salário é de: R$", salario)
